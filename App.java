98a99508290eb0bad9d385842fcd22b366ea6639
riley-hendrickson <rileyhe1@gmail.com>
2025-10-14 18:10:49 -0700

    updating from branch1

--- App.java
+++ App.java
@@ -2,6 +2,8 @@ public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("new version of main");
+        System.out.println("new version from main");
+        System.out.println("new version from branch1");
+        System.out.println("new version from branch2");
     }
 }
