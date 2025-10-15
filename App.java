b3d3d97459d8efea6102baaa06b4c1f41039c595
riley-hendrickson <rileyhe1@gmail.com>
2025-10-14 18:24:58 -0700

    modifying from my desktop

--- App.java
+++ App.java
@@ -2,8 +2,6 @@ public class App
 {
     public static void main(String[] args) 
     {
-        System.out.println("new version from main");
-        System.out.println("new version from branch1");
-        System.out.println("new version from branch2");
+        System.out.println("modified from my desktop");
     }
 }
