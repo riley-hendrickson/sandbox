1564fa0104ba55b96e684f9711ebb21424b5da9c
riley-hendrickson <rileyhe1@gmail.com>
2025-10-14 17:52:19 -0700

    modifiying app from main branch

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App
     public static void main(String[] args) 
     {
         System.out.println("Hello World!");
+        System.out.println("Making change from main branch.");
     }
 }
